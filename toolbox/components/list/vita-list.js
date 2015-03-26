a00d8819a9bc37bfdc412466a75b5dba3b1cc9ee
dustindschultz@gmail.com <dustindschultz@gmail.com>
2015-03-26 10:17:03 -0400

    ic

---
+++ toolbox/components/list/vita-list.js
@@ -0,0 +1,7 @@
+// $( ".listExtender" )
+// 	.css( "background-color", "black" )
+// 	// .css( "height", "100px" )
+// 	// .css( "flex", "1 1 auto" )
+// 	.on( "click", function() {
+// 		console.log( a + a );
+// 	} );
